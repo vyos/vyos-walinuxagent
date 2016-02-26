9afda2f6662f4f90341109d5a8207d6fc14f8707
Ben Howard <ben.howard@ubuntu.com>
2016-02-26 11:06:35 -0700

    Import patches-unapplied version 2.1.3-0ubuntu3 to ubuntu/xenial-proposed
    Imported using git-ubuntu import.
    
    Changelog parent: e509d6821647aee2965c78bbfe8548a66a06ce0d
    
    New changelog entries:
      * Remove conflict with NetworkManager (LP: #1550461):
        - devices on the hyperv bus are ignored via new udev rule.
      * Made the upstart job more consistant with systemd service to use the
        /dev/disk/azure/resource-part1 path instead of /dev/sdb (LP: #1544764)

--- debian/ephemeral-disk-warning.sh
+++ debian/ephemeral-disk-warning.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dev_resource=$(readlink -f /dev/disk/azure/resource-part1)
-dev_resource_mp=$(mount | awk '$1==R {print$3}' "R=${dev_resource}")
+dev_resource_mp=$(awk '$1==R {print$2}' "R=${dev_resource}" /proc/mounts)
 warn_file="${dev_resource_mp}/DATALOSS_WARNING_README.txt"
 
 if [ ! -f "${warn_file}" ]; then
@@ -14,6 +14,10 @@ Please do not use this disk for storing any personal or application data.
 
 For additional details to please refer to the MSDN documentation at:
 http://msdn.microsoft.com/en-us/library/windowsazure/jj672979.aspx
+
+To remove this warning run:
+    sudo chattr -i $warn_file
+
 EOM
 
     chmod 0444 ${warn_file}
